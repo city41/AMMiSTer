179b934270891f7901e8f86862a171c5d2ca029b
Matt Greer <matt.e.greer@gmail.com>
2022-12-23 10:39:07 -0500

    plan

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,9 +1,21 @@
 declare global {
-  interface Window {
-    /** APIs for Electron IPC */
-    ipcAPI?: typeof import('_preload/ipc-api').default
-  }
+	interface Window {
+		/** APIs for Electron IPC */
+		ipcAPI?: typeof import('_preload/ipcAPI').ipcAPI;
+	}
 }
 
+type Game = {
+	name: string;
+	developer: string;
+	orientation: 'horizontal' | 'vertical';
+};
+
+type MRA = {
+	fileName: `${string}.mra`;
+	hash: string;
+	lastUpdated: string;
+};
+
 // Makes TS sees this as an external modules so we can extend the global scope.
-export { };
+export {};
